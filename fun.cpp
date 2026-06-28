dbbcd5453760672d326c094c1bab89e663fe60a6
Matteo7034 <matteo7034@gmail.com>
2026-06-28 21:39:07 +0200

    Message callback

--- fun.cpp
+++ fun.cpp
@@ -1,10 +1,11 @@
 #include "fun.h"
+#include <cstdint>
 #include <iostream>
+#include <vector>
 #include <vulkan/vulkan_core.h>
 
-const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
-};
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
 void HelloTriangleApplication::run() {
   initWindow();
   initVulkan();
@@ -36,23 +37,35 @@ void HelloTriangleApplication::cleanup() {
   glfwDestroyWindow(window);
   glfwTerminate();
 }
-bool HelloTriangleApplication::checkValidationLayerSupport(){
+bool HelloTriangleApplication::checkValidationLayerSupport() {
   uint32_t layerCount;
-  vkEnumerateInstanceLayerProperties(&layerCount,nullptr);
+  vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
   std::vector<VkLayerProperties> availableLayers(layerCount);
-  vkEnumerateInstanceLayerProperties(&layerCount,availableLayers.data());
-  for(const char* layerName : validationLayers){
+  vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+  for (const char *layerName : validationLayers) {
     bool layerFound = false;
-    for(const auto& layerProperties : availableLayers){
-      if(strcmp(layerName, layerProperties.layerName )== 0){
+    for (const auto &layerProperties : availableLayers) {
+      if (strcmp(layerName, layerProperties.layerName) == 0) {
         layerFound = true;
         break;
       }
     }
-    if(!layerFound){
+    if (!layerFound) {
       return false;
     }
   }
   return true;
+}
+
+std::vector<const char *> HelloTriangleApplication::getRequiredExtentions() {
+  uint32_t glfwExtensionCount = 0;
+  const char **glfwExtensions;
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+  std::vector<const char *> extensions(glfwExtensions,
+                                       glfwExtensions + glfwExtensionCount);
+  if (enableValidationLayers) {
+    extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+  }
+  return extensions;
 }
--- fun.h
+++ fun.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
-#include <vector>
-#include <vulkan/vulkan.h>
 #include <cstring>
 #include <stdexcept>
+#include <vector>
+#include <vulkan/vulkan.h>
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
-extern const std::vector<const char*> validationLayers;
+extern const std::vector<const char *> validationLayers;
 
 class HelloTriangleApplication {
 public:
@@ -23,11 +23,10 @@ private:
   bool checkValidationLayerSupport();
   GLFWwindow *window;
   VkInstance instance;
-  std::vector<const char*> getRequiredExtentions();
+  std::vector<const char *> getRequiredExtentions();
 };
 #ifdef NDEBUG
-    const bool enableValidationLayers = false;
+const bool enableValidationLayers = false;
 #else
-    const bool enableValidationLayers = true;
+const bool enableValidationLayers = true;
 #endif
-
